6355619c14606b207365213e7fb7eea9c3441597
PrzeChoj <premysl.choj@gmail.com>
2022-07-01 12:27:28 +0200

    1_tournament

---
+++ project2/src/Game_py/results/README.txt
@@ -0,0 +1,1 @@
+1_tournament_UCT_with_both_heuristics_experiment <<<--- UTC+h was C=sqrt(2); UTC+h_G was C=3.5 G=20
--- project2/src/Game_py/tournament_experiment.py
+++ project2/src/Game_py/tournament_experiment.py
@@ -9,11 +9,9 @@ import math
 from MCTS import MCTS_with_heuristic_h, MCTS_with_heuristic_h_G
 from Node import Node
 
-"""
 # saving to file
 with open('results/1_tournament_UCT_with_both_heuristics_experiment.csv', 'a') as f:
     f.write(f'game_id;seed;who_Green;who_Blue;who_won;moves_made;time\n')
-"""
 
 seed_values = [123, 245, 456, 786, 999, 567, 582, 11, 765, 66]
 time_of_games = []
@@ -24,11 +22,11 @@ for j in range(0, 10):  # 10 repeats experiment
     position.draw_board()
     seed(seed_values[j])  # set seed
     first_player = randint(1, 2)  # choose who starts game
-    print(f"Game starts {'UCT + h_G 3.5' if first_player==1 else 'UCT + h_G 20'}")
+    print(f"Game starts {'UCT + h C=sqrt(2)' if first_player==1 else 'UCT + h_G 20'}")
     while not position.check_is_terminal(print_who_won=False):
         position.calculate_possible_moves()
         if position.get_actual_player() == first_player:
-            engine_mcts = MCTS_with_heuristic_h_G(C=3.5, selection_type="UCT", steps=6, G=3.5)
+            engine_mcts = MCTS_with_heuristic_h(C=math.sqrt(2), selection_type="UCT", steps=6)
         else:
             engine_mcts = MCTS_with_heuristic_h_G(C=3.5, selection_type="UCT", steps=6, G=20)
         num_of_rollouts = 0
@@ -49,14 +47,14 @@ for j in range(0, 10):  # 10 repeats experiment
         position.make_move(Taifho.move_int_to_str(position.get_legal_moves()[engine_move_int]))
         position.draw_board()
     end_game = time.time()
-    print(f"{'MCTS + UCT + h_G heuristics 3.5' if position.winner==(first_player==1) else 'MCTS + UCT + h_G heuristics 20'} ({'Green' if position.winner==(first_player==1) else 'Blue'}) won after {position.moves_made} moves!")
+    print(f"{'MCTS + UCT + h heuristics C=sqrt(2)' if position.winner==(first_player==1) else 'MCTS + UCT + h_G heuristics 20'} ({'Green' if position.winner==(first_player==1) else 'Blue'}) won after {position.moves_made} moves!")
     game_time = end_game - start_game
     time_of_games.append(game_time)
     print(f"Game lasted {game_time} seconds")
 
     # saving to file
     with open('results/1_tournament_UCT_with_both_heuristics_experiment.csv', 'a') as f:
-        f.write(f'{j};{seed_values[j]};{"UCT+h_G3.5" if first_player==1 else "UCT+h_G20"};{"UCT+h_G20" if first_player==1 else "UCT+h_G3.5"};{"UCT+h_G3.5" if position.winner==(first_player==1) else "UCT+h_G20"};{position.moves_made};{game_time}\n')
+        f.write(f'{j};{seed_values[j]};{"UCT+h" if first_player==1 else "UCT+h_G"};{"UCT+h_G" if first_player==1 else "UCT+h"};{"UCT+h" if position.winner==(first_player==1) else "UCT+h_G"};{position.moves_made};{game_time}\n')
 
     print("\n***********************************************")
 mean_time = mean(time_of_games)
